3b8375a8498a1cac19f7b80985245d71ef54f692
ViperLPM <liampat8@hotmail.com>
2026-04-16 23:48:00 -0230

    not too much change but FileNotFoundError is occuring when adding a car after adding style

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-file_save_location = "static/images"
+file_save_location = "CP1890 Project/static/Images"
 allowed_types = [".png", ".jpg", ".webp"]
 
 @app.route("/", methods =["GET"])
@@ -54,6 +54,22 @@ def add():
 def display():
     print(session)
     return render_template('display.html', cars=session.get("cars", []), file_location=file_save_location)
+
+@app.route('/remove')
+def remove():
+    if request.method == "GET":
+        return render_template("remove.html")
+    elif request.method == "POST":
+        if "cars" not in session:
+            print("Clearing data from the session")
+            session["cars"] = []
+        car = request.form.get("car", "invalid")
+        if "cars" in session:
+            for car in session:
+                session["cars"].remove({"car": car})
+        else:
+            flash("This file is of the wrong type", "error")
+            return redirect("./remove")
 
 if __name__ == "__main__":
    app.run(debug=True, host="0.0.0.0")
